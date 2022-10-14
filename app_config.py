0abc38258263cc6589f6d277922381eca80efd1e
ealgera <ericalgera@gmail.com>
2022-10-14 11:56:37 +0200

    Secret to ENV

--- app_config.py
+++ app_config.py
@@ -1,15 +1,15 @@
 import os
+from dotenv import load_dotenv
 
 ### Azure App reg voor: python-auth-code
+load_dotenv(".env")
+
+# App Secrets
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+if not CLIENT_SECRET:
+    raise ValueError("CLIENT_SECRET is niet gedefinieerd!")
 
 CLIENT_ID     = "73de801a-937d-4ee3-ba3e-dec460e1428b"     # Application (client) ID of app registration
-CLIENT_SECRET = "VHe8Q~rZbjvskPJe9T34rCvqvO5BokhpCOrwFaPl" # Placeholder - for use ONLY during testing.
-# In a production app, we recommend you use a more secure method of storing your secret,
-# like Azure Key Vault. Or, use an environment variable as described in Flask's documentation:
-# https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
-# CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-# if not CLIENT_SECRET:
-#     raise ValueError("Need to define CLIENT_SECRET environment variable")
 
 AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app
 # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
